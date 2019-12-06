78012bd32c2dd85a89d43479cca32ee6dfe3007d
coltynw <coltynw@users.github.com>
2019-12-06 00:00:28 -0500

    renders buttons test

--- project/src/App.test.js
+++ project/src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+afterEach(rtl.cleanup);
+
+it('renders without crashing', () => {
+  const wrapper = rtl.render(<App />)
 });
+
+it('renders Buttons', () => {
+  const wrapper = rtl.render(<App />);
+  wrapper.getByText(/^Ball$/i)
+  wrapper.getByText(/^Strike$/i)
+  wrapper.getByText(/^Foul$/i)
+  wrapper.getByText(/^Hit$/i)
+});
+
